92b651f3f154131ec723c6adcd13fdff17c91008
Mithat <mithatsirmali@gmail.com>
2025-06-08 16:53:50 +0300

    Add files via upload

---
+++ LINQ (Language Integrated Query)2/LINQ (Language Integrated Query)2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQ__Language_Integrated_Query_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ to XML
+            XElement kitaplarXml = new XElement("Kitaplar",
+                new XElement("Kitap", new XAttribute("Id", 1), new XElement("Ad", "C# Programlama"), new XElement("Fiyat", 120)),
+                new XElement("Kitap", new XAttribute("Id", 2), new XElement("Ad", "Veritabanı"), new XElement("Fiyat", 90)),
+                new XElement("Kitap", new XAttribute("Id", 3), new XElement("Ad", "Yapay Zeka"), new XElement("Fiyat", 150))
+            );
+
+            var pahaliKitaplar = from kitap in kitaplarXml.Elements("Kitap")
+                                 where (int)kitap.Element("Fiyat") > 100
+                                 select kitap.Element("Ad").Value;
+
+            Console.WriteLine("Fiyatı 100 TL'den fazla olan kitaplar (LINQ to XML):");
+            foreach (var ad in pahaliKitaplar)
+                Console.WriteLine(ad);
+
+            // LINQ to SQL (sadece örnekleme)
+            // var db = new VeritabaniContext();
+            // var kullanicilar = from k in db.Kullanicilar
+            //                    where k.Yas > 18
+            //                    select k.Isim;
+
+            // foreach (var isim in kullanicilar)
+            //     Console.WriteLine(isim);
+
+            Console.WriteLine("\n(Not: LINQ to SQL için gerçek bir veritabanı bağlantısı gereklidir.)");
+        }
+    }
+}
---
+++ LINQ (Language Integrated Query)2/LINQ (Language Integrated Query)2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LINQ (Language Integrated Query)2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LINQ (Language Integrated Query)2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4ac22c9-3d6d-4ac0-ab72-0ceafc066710")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
